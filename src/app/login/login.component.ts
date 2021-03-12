cdece159a6ffc785a55d6ff855a242cdb2b80878
Pablo Sanchez <pablo@transifex.com>
2021-03-12 12:12:37 +0200

    Add decorator

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslationService } from '@transifex/angular';
+import { TranslatableString, TranslationService } from '@transifex/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,20 +7,25 @@ import { TranslationService } from '@transifex/angular';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  // Translations using decorator
+  @TranslatableString('Monday', { _key: 'text.monday' })
   _weekday: string;
+  @TranslatableString('terms of service', { _key: 'text.terms_of_service' })
   _terms: string;
+  @TranslatableString('privacy policy', { _key: 'text.privacy_policy' })
   _privacy: string;
 
   constructor(private translationService: TranslationService) {
-    const that = this;
-    this.translationService.localeChanged.subscribe(res => {
-      that._weekday = this.translationService.translate('Monday',
-        { _key: 'text.monday' });
-      that._terms = this.translationService.translate('terms of service',
-        { _key: 'text.terms_of_service' });
-      that._privacy = this.translationService.translate('privacy policy',
-        { _key: 'text.privacy_policy' });
-    });
+    // Translations using translation service
+    // const that = this;
+    // this.translationService.localeChanged.subscribe(res => {
+    //   that._weekday = this.translationService.translate('Monday',
+    //     { _key: 'text.monday' });
+    //   that._terms = this.translationService.translate('terms of service',
+    //     { _key: 'text.terms_of_service' });
+    //   that._privacy = this.translationService.translate('privacy policy',
+    //     { _key: 'text.privacy_policy' });
+    // });
   }
 
   ngOnInit(): void { }
